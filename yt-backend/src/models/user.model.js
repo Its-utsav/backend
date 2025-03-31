9dc0ec771563c1d28c99d8ade4a472648e1e3e5e
its-utsav <utsavdhimmar987@gmail.com>
2025-03-31 07:51:06 +0000

    complete refresh and access token

--- yt-backend/src/models/user.model.js
+++ yt-backend/src/models/user.model.js
@@ -51,7 +51,7 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-    // we need to hash password only when password is modify or add first time
+    // we need to hash password only when password is added first time or only modified
     // password encyrption takes time
     if (!this.isModified("password")) return next();
     this.password = bcrypt.hash(this.password, 10);
@@ -60,6 +60,33 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            userName: this.username,
+            fullName: this.fullName,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN,
+        {
+            expiresIn: process.env.REFERSH_TOKEN,
+        }
+    );
+};
+
+userSchema.methods.generateRefershToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFERSH_TOKEN,
+        {
+            expiresIn: process.env.REFERSH_TOKEN_EXPIRY,
+        }
+    );
 };
 
 const User = model("User", userSchema);
